242a3563d0bae4c9f0f95eb5bd76e198ea05285f
Cedric <cedric.balmer@gmx.ch>
2019-09-18 11:40:24 -0700

    added script.js and completed it, but not working properly

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("cookie").innerHTML = document.cookie;
+
+document.getElementById("baseURI").innerHTML = document.baseURI;
+
+document.getElementById("docType").innerHTML = documet.doctype;
+
+document.getElementById("documentMode").innerHTML = document.documentMode;
+
+document.getElementById("implementation").innerHTML = document.implementation;
+
+document.getElementById("inputEncoding").innerHTML = document.inputEncoding;
+
+document.getElementById("ErrorChecking").innerHTML = document.strictErrorChecking;
+
+document.getElementById("titleName").innerHTML = document.title;
